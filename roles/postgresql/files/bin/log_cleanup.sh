adb1ffd94abe59cb158766b17c14bf980a40b9ca
Sebastiaan Mannem <sebastiaan.mannem@rivm.nl>
2022-02-09 13:35:15 +0100

    log cleanup voor postgres

---
+++ roles/postgresql/files/bin/log_cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+RETAIN=$1
+ZIP=$2
+LOGDIR=$3
+
+# log output to a logfile in the logdir
+exec > "$LOGDIR/$(basename $0 .sh).log"
+
+echo "Deleting all files in '$LOGDIR' older than $RETAIN days."
+find "$LOGDIR" -mtime "+$RETAIN" -delete
+
+echo "Zipping all non-gzip files in '$LOGDIR' older than $ZIP days."
+find "$LOGDIR" -mtime "+$ZIP" | while read f; do
+  [[ "$f" =~ .*\.gz ]] && continue
+  echo "gzip $f"
+  gzip $f
+done
