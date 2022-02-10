45e25f389d26ce50faf2a2f010971811f47de22a
Sebastiaan Mannem <sebastiaan.mannem@rivm.nl>
2022-02-10 15:00:00 +0100

    WHoops kleine issues in de extra scripts. Gefixt.

--- roles/stolon/files/scripts/demote.sh
+++ roles/stolon/files/scripts/demote.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo 'Sourcing stolonctl parameters (from /etc/sysconfig/stolon-stsentinel)'
+eval $(sed -e '/#/d;s/^STSENTINEL/export STOLONCTL/' /etc/sysconfig/stolon-stsentinel )
+
 echo 'Switchover to other node if this is the master'
 /usr/local/bin/stolonctl failkeeper $(hostname | tr '-' '_')
--- roles/stolon/files/scripts/reinstate.sh
+++ roles/stolon/files/scripts/reinstate.sh
@@ -6,7 +6,7 @@ systemctl stop stolon-keeper.service
 
 echo 'Sourcing stolonctl parameters (from /etc/sysconfig/stolon-stkeeper)'
 # Note that this command also sources $STKEEPER_DATA_DIR, renamed as $STOLONCTL_DATA_DIR
-eval $(sed -e '/#/d;s/^STKEEPER/export STOLONCTL/' /etc/sysconfig/stolon-stsentinel )
+eval $(sed -e '/#/d;s/^STKEEPER/export STOLONCTL/' /etc/sysconfig/stolon-stkeeper )
 echo 'Removing local keeper from etcd'
 /usr/local/bin/stolonctl removekeeper $(hostname | tr '-' '_')
 
