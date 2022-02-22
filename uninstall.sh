39a0f8da1d1c1df328b97813d340d7b4355fd046
Sebastiaan Mannem <sebastiaan.mannem@rivm.nl>
2022-02-22 19:42:25 +0100

    Wat aanpassingen op uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,16 @@
 ---
-- name: "Gather facts on all servers"
+- name: "Run the playbook"
   hosts:
     - all
+  vars_prompt:
+    - name: sure
+      prompt: "This will remove postgres and all backups. Are you really sure? Type 'yes' in uppercase if you are..."
+      private: no
   become: true
   tasks:
+    - name: "Are you sure?"
+      assert:
+        that:
+          - "sure == 'YES'"
     - name: "Run uninstall"
       script: uninstall.sh
