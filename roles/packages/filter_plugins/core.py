c640890513cfd0c06e2cf0fb07f00872dd4a010b
Sebastiaan Mannem <sebastiaan.mannem@rivm.nl>
2022-02-22 10:48:39 +0100

    Nieuwe rol die in 1 klap alle nodige packages installeert Misschien dat we in de toekomst dit wel binnen de andererollen laten... Ook geen subscriptie gedoe in de postgres rol. Verhuisd naar de packages rol.

---
+++ roles/packages/filter_plugins/core.py
@@ -0,0 +1,9 @@
+def filter_by_group(grouped_lists, groups):
+    # Use a set to make uniue, and then convert to list again to make it json_serializable
+    return list({ value for key, values in grouped_lists.items() if key in groups for value in values })
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'bygroup': filter_by_group
+        }
