c38a6174e48b1fc3f70c1fef055e86da511d6002
Sebastiaan Mannem <sebastiaan.mannem@rivm.nl>
2022-05-09 16:15:49 +0200

    avchecker.py met een configreerbare timeout. En last tabel herboemd naar avchecker

--- roles/avchecker/files/avchecker.py
+++ roles/avchecker/files/avchecker.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
+import os
 import psycopg2
 import time
 cn=None
 last = None
+SLEEPTIME=float(os.environ.get('AVCHECKER_SLEEPTIME', '5'))
 while True:
     try:
-        time.sleep(.1)
+        time.sleep(SLEEPTIME)
         if not cn:
             cn = psycopg2.connect('')
             cur = cn.cursor()
-            cur.execute('select count(*) from pg_class where relname = %s and relnamespace in (select oid from pg_namespace where nspname=%s) ', ('last', 'public'))
+            cur.execute('select count(*) from pg_class where relname = %s and relnamespace in (select oid from pg_namespace where nspname=%s) ', ('avchecker', 'public'))
             if next(cur)[0] == 0:
                 print('Creating table')
-                cur.execute('create table public.last(t timestamp)')
-                cur.execute('insert into public.last values(now())')
+                cur.execute('create table public.avchecker(last timestamp)')
+                cur.execute('insert into public.avchecker values(now())')
         cur.execute('BEGIN')
-        cur.execute('update public.last set t = now()')
+        cur.execute('update public.avchecker set last = now()')
         cur.execute('COMMIT')
-        cur.execute('select t from public.last')
+        cur.execute('select last from public.avchecker')
         row=next(cur)
         new = row[0]
         if last:
             delta = new-last
-            if delta.total_seconds() >= .15:
+            if delta.total_seconds() >= (SLEEPTIME*1.5):
                 print(delta, flush=True)
         last = new
     except (psycopg2.InternalError, psycopg2.OperationalError, psycopg2.DatabaseError) as err:
         print(str(err).split('\n')[0])
         cn = None
-
