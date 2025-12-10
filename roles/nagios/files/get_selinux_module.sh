b952c0cdf27acb42f733c5ca13a8a2be05c0af7a
Sebastiaan Mannem <sebas@mannem.nl>
2025-12-10 23:11:49 +0100

    changing to galaxy roles

--- roles/nagios/files/get_selinux_module.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd $(mktemp -d)
-echo "Running from $PWD"
-sealert -l "*" | sed -n '/^# ausearch/{s/^# //;p}' | sort -u > get_audits.sh
-bash ./get_audits.sh
-cat *.te > mymodule.te
--- roles/walg/files/scripts/archive_restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# WAL-g config laden
-eval "$(sed '/#/d;s/^/export /' /etc/default/wal-g)"
-
-/usr/local/bin/wal-g-pg wal-fetch "$1" "$2"
